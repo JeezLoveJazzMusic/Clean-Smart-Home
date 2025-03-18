de3e1ae5b1f3f6ec2e5e4b596d9331d8a63a287f
Joe <wo918812@gmail.com>
2025-03-19 00:07:15 +0800

    Fixed
    '  issue

--- Client-side/src/Components/UserPermissions/UserPermissions.jsx
+++ Client-side/src/Components/UserPermissions/UserPermissions.jsx
@@ -14,7 +14,7 @@ const UserPermissions = ({ user, onClose, currentRoom, houseId }) => {
       console.log("UserPermissions: currentRoom:", currentRoom, "HouseId:", houseId, "User:", user.id);
       try {
         const response = await axios.get(
-          http://localhost:8080/getUserPermissionForRoom/house/${houseId}/user/${user.id}/room/${currentRoom}
+          `http://localhost:8080/getUserPermissionForRoom/house/${houseId}/user/${user.id}/room/${currentRoom}`
         );
         const { userPermission } = response.data;
         // Convert the returned array into a mapping, e.g. [95] becomes { 95: true }
@@ -42,7 +42,7 @@ const UserPermissions = ({ user, onClose, currentRoom, houseId }) => {
     const fetchDevices = async () => {
       try {
         const response = await axios.get(
-          http://localhost:8080/getRoomDevices/houses/${houseId}/rooms/${currentRoom}
+          `http://localhost:8080/getRoomDevices/houses/${houseId}/rooms/${currentRoom}`
         );
         const { devices } = response.data;
         console.log("Fetched devices for permissions:", devices);
@@ -99,7 +99,7 @@ const UserPermissions = ({ user, onClose, currentRoom, houseId }) => {
       if (removePermissionIds.length > 0) {
         await Promise.all(
           removePermissionIds.map((deviceId) =>
-            axios.delete(http://localhost:8080/remove_permission/user/${user.id}/device/${deviceId})
+            axios.delete(`http://localhost:8080/remove_permission/user/${user.id}/device/${deviceId}`)
           )
         );
         console.log("Removed permissions for:", removePermissionIds);
